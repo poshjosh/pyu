2e383a9516cf1dbeda1c23fd36d031ab9ec7bf20
Chinomso Ikwuagwu <chinomso.ikwuagwu@unite.eu>
2024-05-24 21:14:42 +0200

    Add CHANGELOG.md, update project.toml, setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,20 @@ from setuptools import setup, find_packages
 
 if __name__ == "__main__":
     setup(name="pyu",
-          version="0.1.0",
+          version="0.1.1",
+          description="Python utilities for common tasks",
+          author="PyU Team",
+          author_email="posh.bc@gmail.com",
+          install_requires=["PyYAML==6.0.1"],
+          classifiers=[
+              "Programming Language :: Python :: 3",
+              "License :: OSI Approved :: MIT License",
+              "Operating System :: OS Independent",
+          ],
+          url="https://github.com/poshjosh/pyu",
           packages=find_packages(
               where='src',
               include=['pyu'],
           ),
           package_dir={"": "src"},
-          install_requires=["PyYAML==6.0.1"],
           )
