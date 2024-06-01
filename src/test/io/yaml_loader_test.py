1d8a79e9844b3311acf87bcd094e45f5d9e9d5b8
Chinomso Ikwuagwu <chinomso.ikwuagwu@unite.eu>
2024-06-01 17:31:34 +0200

    Make test location agnostic

--- src/test/io/yaml_loader_test.py
+++ src/test/io/yaml_loader_test.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 
 from pyu.io.yaml_loader import YamlLoader
@@ -5,7 +6,8 @@ from pyu.io.yaml_loader import YamlLoader
 
 class YamlLoaderTestCase(unittest.TestCase):
     def test_load_config(self):
-        config: dict = YamlLoader(".", suffix=".config").load_config("yaml_loader_test")
+        curr_dir = os.path.dirname(__file__)
+        config: dict = YamlLoader(curr_dir, suffix=".config").load_config("yaml_loader_test")
         print(f"Config: {config}")
         self.assertEqual(config['str'], 'Test')
         self.assertTrue(config['bool'])
